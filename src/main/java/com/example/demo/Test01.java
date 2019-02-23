023e5681cf30fc70b527cdde3eeace95ec1df8c4
sugarkissme <33215505+sugarkissme@users.noreply.github.com>
2019-02-23 21:29:18 +0800

    Update Test01.java

--- src/main/java/com/example/demo/Test01.java
+++ src/main/java/com/example/demo/Test01.java
@@ -1,1 +1,13 @@
 
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
